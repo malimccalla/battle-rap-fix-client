e3678eeb729922b7e0a4eb220580553e7caed187
Terry Tilley <terry@terrytilley.com>
2017-12-31 20:32:02 +0000

    wrapped Router Switch with Layout component

--- src/routes/Artists/index.js
+++ src/routes/Artists/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { graphql } from 'react-apollo';
 
-import Layout from '../../components/Layout';
 import Card from '../../components/Card';
 import Col from '../../components/common/Grid/Column';
 import { Container, Row } from '../../components/common/Grid';
@@ -11,21 +10,19 @@ import { Container, Row } from '../../components/common/Grid';
 import query from '../../queries/allArtists';
 
 const Artists = ({ data: { artists = [] } }) => (
-  <Layout>
-    <Container>
-      <Row>
-        <Col size={6} md>
-          <h1>Artists</h1>
-        </Col>
-        <Col size={6} md>
-          <Link to="/artists/new">Add Artist</Link>
-        </Col>
-      </Row>
-      <Card>
-        <ul>{artists.map(u => <li key={u.id}>{u.stageName}</li>)}</ul>
-      </Card>
-    </Container>
-  </Layout>
+  <Container>
+    <Row>
+      <Col size={6} md>
+        <h1>Artists</h1>
+      </Col>
+      <Col size={6} md>
+        <Link to="/artists/new">Add Artist</Link>
+      </Col>
+    </Row>
+    <Card>
+      <ul>{artists.map(u => <li key={u.id}>{u.stageName}</li>)}</ul>
+    </Card>
+  </Container>
 );
 
 Artists.propTypes = {
--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import Layout from '../components/Layout';
 import Card from '../components/Card';
 import { Container } from '../components/common/Grid';
 
 const Dashboard = () => (
-  <Layout>
-    <Container>
-      <h1>Dashboard</h1>
-      <Card />
-    </Container>
-  </Layout>
+  <Container>
+    <h1>Dashboard</h1>
+    <Card />
+  </Container>
 );
 
 export default Dashboard;
